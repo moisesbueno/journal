661c48c3c3526a2aaa7bdcc598fe7f343e5a9d99
Moisés Bueno <moisesbueno@ioasys.com.br>
2025-03-24 10:05:56 -0300

    implementa cqrs

---
+++ backend/Journal.Application/DTOs/JournalAddRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace Journal.Application.DTOs
+{
+    public class JournalAddRequest
+    {
+        public string Issn { get; set; }
+        public string Name { get; set; }
+        public string Qualis { get; set; }
+    }
+}
